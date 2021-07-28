3ec09f09c5a9ec8729245b22b208ecdb1e9e793b
Joe <joseph.allen.1994@hotmail.com>
2021-07-28 18:46:58 +0100

    Add ex5 covering reading in a file

--- exercises/exc_01_04.R
+++ exercises/exc_01_04.R
@@ -3,16 +3,3 @@ total <- ____
 
 # Print the variable
 print(____)
-
-# test read in the dataset from /data/test.csv
-df <- read.csv2("data/test.csv", 
-               header= FALSE)
-
-df
-
-# read from web hosted files
-df <- read.csv2("https://s3.amazonaws.com/assets.datacamp.com/blog_assets/test.csv", 
-               header= FALSE)
-
-# print out dataframe
-df
---
+++ exercises/exc_01_05.R
@@ -0,0 +1,5 @@
+# test read in the dataset from /data/test.csv
+df <- read.csv2("data/test.csv", 
+               header= FALSE)
+
+df
