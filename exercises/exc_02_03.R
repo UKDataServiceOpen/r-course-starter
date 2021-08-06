f6f010285936eddf035ee7d742798102e20abbb7
Joe <joseph.allen.1994@hotmail.com>
2021-08-06 09:32:45 +0100

    Add Haven demos

--- exercises/exc_02_03.R
+++ exercises/exc_02_03.R
@@ -1,2 +1,5 @@
+library (haven)
+ntsl3 <- haven::read_sav("data/natsal_3_teaching_open.sav")
+
 hist(ntsl3$dage1ch)
 summary(ntsl3$dage1ch, na.rm = T)
--- exercises/exc_02_04.R
+++ exercises/exc_02_04.R
@@ -1,5 +1,8 @@
+library (haven)
 library(ggplot2)
 library(dplyr)
+ntsl3 <- haven::read_sav("data/natsal_3_teaching_open.sav")
+
 #convert sex into factor
 ntsl3$sex <- as_factor(ntsl3$rsex)  
 
